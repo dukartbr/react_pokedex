1c73665b250bcbafaa30d5dd6097869f74de6c0d
Brady Dukart <bdukart@bushelpowered.com>
2020-01-10 21:36:57 -0600

    refactored pokemon cards and axios requests

--- src/Components/pokemon.js
+++ src/Components/pokemon.js
@@ -1,126 +1,126 @@
-import React, { Component } from 'react';
-import Axios from 'axios';
-import PokemonCard from './pokemonCard';
-import Welcome from './Welcome';
-import PokedexLeftHeader from './PokedexLeftHeader';
-import PartyItem from './PartyItem';
+import React, { Component } from "react";
+import Axios from "axios";
+import PokemonCard from "./pokemonCard";
+import Welcome from "./Welcome";
+import PokedexLeftHeader from "./PokedexLeftHeader";
+// import PartyItem from "./PartyItem";
 
 class Pokedex extends Component {
-    state = {
-        pokemons: [],
-        pokemonID: [],
-        pokemonName: null,
-        pokemonAbilites: [],
-        pokemonAbilityDescriptions: [],
-        pokemonMoves: [],
-        pokemonImages: {},
-        pokemonStats: [],
-        pokemonTypes: [],
-        party: [],
-        isInParty: false,
-        showPokemonCard: false
-    }
+  state = {
+    pokemons: [],
+    pokemonID: [],
+    pokemonName: null,
+    pokemonAbilites: [],
+    pokemonAbilityDesciptionURLS: [],
+    pokemonAbilityDescriptions: [],
+    pokemonMoves: [],
+    pokemonImages: {},
+    pokemonStats: [],
+    pokemonTypes: [],
+    party: [],
+    isInParty: false,
+    showPokemonCard: false
+  };
 
-    componentDidMount() {
-        Axios.get('https://pokeapi.co/api/v2/pokemon?limit=151').then((response)=> {
-            this.setState({
-                pokemons: response.data.results
-            })
-        })
-    }
+  componentDidMount() {
+    Axios.get("https://pokeapi.co/api/v2/pokemon?limit=151").then(response => {
+      this.setState({
+        pokemons: response.data.results
+      });
+    });
+  }
 
-    renderCard = (pokemon) => {
-        Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`).then( response => {
-            const res = response.data;
-            let abilityDescriptions = [];
-            res.abilities.map(ability => {
-                const url = ability.ability.url;
-                Axios.get(url).then(response => {
-                    response.data.effect_entries.map(effect => {
-                        abilityDescriptions.push(effect.short_effect);
-                    });
-                })
-            })
-            this.setState({
-                pokemonID: res.id,
-                pokemonName: res.name,
-                pokemonAbilites: res.abilities,
-                pokemonAbilityDescriptions: abilityDescriptions,
-                pokemonMoves: res.moves,
-                pokemonImages: res.sprites,
-                pokemonStats: res.stats,
-                pokemonTypes: res.types,
-                showPokemonCard: true
-            });
-        })
-    }
-
-    addToParty = (pokemon) => {
+  renderCard = pokemon => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`).then(
+      response => {
+        const res = response.data;
         this.setState({
-            ...this.state,
-            pokemon
-        })
-    }
+          pokemonID: res.id,
+          pokemonName: res.name,
+          pokemonAbilites: res.abilities,
+          pokemonMoves: res.moves,
+          pokemonImages: res.sprites,
+          pokemonStats: res.stats,
+          pokemonTypes: res.types,
+          showPokemonCard: true
+        });
+      }
+    );
+  };
 
-    render() {
-        const { pokemons, pokemonID, pokemonName, pokemonAbilites, pokemonAbilityDescriptions ,pokemonMoves, pokemonImages, pokemonStats, pokemonTypes, Party, isFavorited } = this.state;
-        return (
-            <div className="row">
-                <div className="col-6 remove-padding">
-                    <div className="left-container">
-                        <div className="left-container--border">
-                            <PokedexLeftHeader />
-                            <div className="pokemonList--container__padding">
-                                <div className="pokemonList--container">
-                                    {pokemons.map(pokemon => (
-                                        <button key={pokemon.name} onClick={() => this.renderCard(pokemon)}>{pokemon.name}</button>
-                                    ))}
-                                </div>
-                            </div>
-                            <div className="favorites--container">
-                                <div className="container">
-                                    <div className="row">
-                                        <p className="favorites--header">Party</p>
-                                        <PartyItem      pokemonImages={pokemonImages}/>
-                                        <PartyItem      pokemonImages={pokemonImages}/>
-                                        <PartyItem      pokemonImages={pokemonImages}/>
-                                        <PartyItem      pokemonImages={pokemonImages}/>
-                                        <PartyItem      pokemonImages={pokemonImages}/>
-                                        <PartyItem      pokemonImages={pokemonImages}/>
-                                    </div>
+  addToParty = pokemon => {
+    this.setState({
+      ...this.state,
+      pokemon
+    });
+  };
 
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
+  render() {
+    const {
+      pokemons,
+      pokemonID,
+      pokemonName,
+      pokemonAbilites,
+      pokemonMoves,
+      pokemonImages,
+      pokemonStats,
+      pokemonTypes,
+      Party,
+      isFavorited
+    } = this.state;
+    return (
+      <div className="row">
+        <div className="col-6 remove-padding">
+          <div className="left-container">
+            <div className="left-container--border">
+              <PokedexLeftHeader />
+              <div className="pokemonList--container__padding">
+                <div className="pokemonList--container">
+                  {pokemons.map(pokemon => (
+                    <button
+                      key={pokemon.name}
+                      onClick={() => this.renderCard(pokemon)}
+                    >
+                      {pokemon.name}
+                    </button>
+                  ))}
                 </div>
-                <div className="col-6 remove-padding">
-                    <div className="right-container">
-                        <div className="right-container--border">
-                            {
-                                this.state.showPokemonCard ?
-                                <PokemonCard 
-                                    pokemonID={pokemonID}
-                                    pokemonName={pokemonName}
-                                    pokemonAbilites={pokemonAbilites}
-                                    pokemonAbilityDescriptions={pokemonAbilityDescriptions}
-                                    pokemonMoves={pokemonMoves}
-                                    pokemonImages={pokemonImages}
-                                    pokemonStats={pokemonStats}
-                                    pokemonTypes={pokemonTypes}
-                                    party={Party}
-                                    isFavorited={isFavorited}
-                                    FavoriteHandler={this.addToParty}
-                                />
-                                : <Welcome />
-                            }
-                        </div>
-                    </div>
+              </div>
+              <div className="favorites--container">
+                <div className="container">
+                  <div className="row">
+                    <p className="favorites--header">Party</p>
+                  </div>
                 </div>
+              </div>
             </div>
-        );
-    }
+          </div>
+        </div>
+        <div className="col-6 remove-padding">
+          <div className="right-container">
+            <div className="right-container--border">
+              {this.state.showPokemonCard ? (
+                <PokemonCard
+                  pokemonID={pokemonID}
+                  pokemonName={pokemonName}
+                  pokemonAbilites={pokemonAbilites}
+                  pokemonMoves={pokemonMoves}
+                  pokemonImages={pokemonImages}
+                  pokemonStats={pokemonStats}
+                  pokemonTypes={pokemonTypes}
+                  party={Party}
+                  isFavorited={isFavorited}
+                  FavoriteHandler={this.addToParty}
+                />
+              ) : (
+                <Welcome />
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Pokedex;
--- src/Components/pokemonCard.js
+++ src/Components/pokemonCard.js
@@ -1,103 +1,107 @@
-import React, { Component } from 'react';
-import Axios from 'axios';
+import React, { Component } from "react";
+import Axios from "axios";
 
 class pokemonCard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isFavorited: false,
-            abilityDescriptions: [],
-            pokemonName: null
-        }
-    }
+  state = {
+    isFavorited: false,
+    abilityDescriptions: []
+  };
 
-    toggleFavoritesHandler = () => {
-        this.setState({
-            abilityDescriptions: this.props.pokemonAbilityDescriptions
-        }, console.log('hello'))
-    }
+  getAbilityURLs = pokemon => {
+    const abilityDescriptions = [];
+    pokemon.pokemonAbilites.map(pokemon => {
+      const url = pokemon.ability.url;
+      Axios.get(`${url}`).then(response => {
+        const res = response.data.effect_entries[0].short_effect;
+        abilityDescriptions.push(res);
+      });
+    });
+  };
 
-    componentDidUpdate = (prevProps, prevState) => {
-        console.log('component updated')
-        let abilities = this.props.pokemonAbilityDescriptions;
-        if (prevState && prevState.abilityDescriptions !== abilities) {
-            this.setState({
-                abilityDescriptions: abilities
-            })
-        }
-    }
-
-    getItemUrl = (url) => {
-        Axios.get(url).then((res) => {
-            const effect = res.data.effect_entries[0].effect;
-            return effect;
-        })
-    }
-
-    render() {
-        const {pokemonAbilites, pokemonAbilityDescriptions, pokemonName, pokemonMoves, pokemonImages, pokemonStats, pokemonTypes, pokemonID} = this.props;
-        const {abilityDescriptions} = this.state;
-        return (
-            <div className="container">
-            <div className="pokemonCard--container">
-                <div className="pokemonCard--header">
-                    <h2>{pokemonName}</h2>
-                    {pokemonTypes.map(pokemon => (
-                        <span className="pokemonCard--type" key={pokemon.type.name}>{pokemon.type.name}</span>
+  render() {
+    const {
+      pokemonAbilites,
+      pokemonName,
+      pokemonMoves,
+      pokemonImages,
+      pokemonStats,
+      pokemonTypes,
+      pokemonID
+    } = this.props;
+    console.log(this.getAbilityURLs(this.props));
+    return (
+      <div className="container">
+        <div className="pokemonCard--container">
+          <div className="pokemonCard--header">
+            <h2>{pokemonName}</h2>
+            {pokemonTypes.map(pokemon => (
+              <span className="pokemonCard--type" key={pokemon.type.name}>
+                {pokemon.type.name}
+              </span>
+            ))}
+          </div>
+          <div className="pokemonCard--screen">
+            {pokemonName ? (
+              <button
+                onClick={() => this.toggleFavoritesHandler(this.props)}
+                className="pokemonCard--favorite"
+              >
+                <i className="fas fa-star"></i>
+              </button>
+            ) : null}
+            <div className="row">
+              <div className="col-3">
+                <img src={pokemonImages.front_default} alt="" />
+              </div>
+              <div className="col-8">
+                <div className="pokemonCard--stat-container">
+                  <h3>Stats</h3>
+                  <ul>
+                    {pokemonStats.map(pokemon => (
+                      <li key={pokemon.stat.name}>
+                        {pokemon.stat.name} :{" "}
+                        <span className="pokemonCard--base-stat">
+                          {pokemon.base_stat}
+                        </span>
+                      </li>
                     ))}
+                  </ul>
                 </div>
-                <div className="pokemonCard--screen">
-                    {pokemonName ?
-                        <button onClick={()=> this.toggleFavoritesHandler(this.props)} className="pokemonCard--favorite">
-                            <i className="fas fa-star"></i>
-                        </button>
-                    :
-                        null
-                    }
-                    <div className="row">
-                        <div className="col-3">
-                            <img src={pokemonImages.front_default} alt="" />
-                        </div>
-                        <div className="col-8">
-                            <div className="pokemonCard--stat-container">
-                                <h3>Stats</h3>
-                                <ul>
-                                    {pokemonStats.map(pokemon => (
-                                        <li key={pokemon.stat.name}>{pokemon.stat.name} : <span className="pokemonCard--base-stat">{pokemon.base_stat}</span></li>
-                                    ))}
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <span className="pokemonCard--pokemonID">ID: {pokemonID}</span>
-                </div>
-                <div className="row">
-                    <div className="col-12">
-                        <h3>Abilities</h3>
-                        <ul>
-                            {pokemonAbilites.map((pokemon, i) => {
-                                return (
-                                    <li key={i}>{pokemon.ability.name} - {abilityDescriptions[i]}</li>
-                                )
-                            })}
-                        </ul>
-
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="pokemonCard--moves-container">
-                        <h3>Moves</h3>
-                        <ul>
-                            {pokemonMoves.slice(0, 4).map(pokemon => (
-                                <li key={pokemon.move.name} className="pokemonCard--moves-item"><span className="pokemonCard--moves-title">{pokemon.move.name}</span></li>
-                            ))}
-                        </ul>
-                    </div>
-                </div>
+              </div>
             </div>
+            <span className="pokemonCard--pokemonID">ID: {pokemonID}</span>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <h3>Abilities</h3>
+              <ul>
+                {pokemonAbilites.map((pokemon, i) => {
+                  return <li key={i}>{pokemon.ability.name} -</li>;
+                })}
+              </ul>
+            </div>
+          </div>
+          <div className="row">
+            <div className="pokemonCard--moves-container">
+              <h3>Moves</h3>
+              <ul>
+                {pokemonMoves.slice(0, 4).map(pokemon => (
+                  <li
+                    key={pokemon.move.name}
+                    className="pokemonCard--moves-item"
+                  >
+                    <span className="pokemonCard--moves-title">
+                      {pokemon.move.name}
+                    </span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
-        );
-    }
+      </div>
+    );
+  }
 }
 
 export default pokemonCard;
