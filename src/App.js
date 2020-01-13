e58e3286573537e38bcfe50580ee18159ed47971
Brady Dukart <bdukart@bushelpowered.com>
2020-01-13 14:13:57 -0600

    beginning of redux implementation, will get back to this part

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import './App.scss';
-import Pokedex from './Components/pokemon';
+import React from "react";
+import "./App.scss";
+import Pokedex from "./Components/pokemon";
+// import { Provider } from "react-redux";
+// import store from "./store";
 
 function App() {
   return (
+    // <Provider store={store}>
     <div className="App">
       <div className="pokedex--header">
         <h1>Pokédex</h1>
@@ -12,6 +15,7 @@ function App() {
         <Pokedex />
       </div>
     </div>
+    // </Provider>
   );
 }
 
---
+++ src/api.js
@@ -0,0 +1,6 @@
+// Generations
+
+export const gen1 = '/api/v2/generation/1/';
+export const gen2 = '/api/v2/generation/2/';
+export const gen3 = '/api/v2/generation/3/';
+export const gen4 = '/api/v2/generation/4/';
